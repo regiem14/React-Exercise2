52132decc6ff1a7669056d8c78280891c00bc003
Regie Marzan <lonebeginner@gmail.com>
2023-04-17 18:57:00 +0800

    add const a & b inside the h2

--- creatapp1/src/App.js
+++ creatapp1/src/App.js
@@ -4,7 +4,7 @@
 function App() {
   const name =  "regie";
   
-  // const a = true;
+  const a = false;
   // const b = 5+5;
   return (
     
@@ -12,6 +12,7 @@ function App() {
       <h1>Hello World. Reacts</h1>
     <h2>Welcome {name} to our react crash course</h2>
     <h2 className="App">Hi. This an h2 in React!</h2>
+    <h2>I am new to JSX & I love it {a ? 'yes': 'no'} </h2>
     </div>
     // <div className="contianer">
     //   <Header />
