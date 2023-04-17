70bc8dbe8520b68663c1a188ed6401453b6b5771
Regie Marzan <lonebeginner@gmail.com>
2023-04-17 19:23:32 +0800

    add components folder & googlefont

--- creatapp1/src/App.js
+++ creatapp1/src/App.js
@@ -4,15 +4,17 @@
 function App() {
   const name =  "regie";
   
-  const a = false;
-  // const b = 5+5;
+  const a = true;
+  const b = 5+5;
   return (
     
-    <div className="App">
+    <div className="container">
       <h1>Hello World. Reacts</h1>
     <h2>Welcome {name} to our react crash course</h2>
     <h2 className="App">Hi. This an h2 in React!</h2>
     <h2>I am new to JSX & I love it {a ? 'yes': 'no'} </h2>
+    <h2>{5+5}</h2>
+    <h2>{b}</h2>
     </div>
     // <div className="contianer">
     //   <Header />
